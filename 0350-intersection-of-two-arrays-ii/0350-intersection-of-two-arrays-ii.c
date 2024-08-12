36939808de6ed47cb72c8566701cb79d8a325516
Tharaniguru <125856377+Tharaniguru@users.noreply.github.com>
2024-08-12 17:41:13 +0530

    Time: 10 ms (9.42%), Space: 9.8 MB (10.60%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.c
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.c
@@ -5,9 +5,7 @@ int* intersect(int* nums1, int n1, int* nums2, int n2, int* returnSize) {
     int *arr=(int*)malloc(sizeof(int)*(n1<n2?n2 :n1));
     int k=0;
     for(int i=0;i<n1;i++){
-        if(nums1[i]==-1){
-            continue;
-        }
+        
         for(int j=0;j<n2;j++){
             if(nums1[i]==nums2[j]){
                 arr[k]=nums1[i];
