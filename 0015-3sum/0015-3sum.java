0ad9cd2ef56f08dee63fc8dd96b04da53c61ad6e
Tharaniguru <125856377+Tharaniguru@users.noreply.github.com>
2024-09-20 18:12:34 +0530

    Time: 36 ms (36.19%), Space: 52.1 MB (35.86%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,41 +1,52 @@
-import java.util.*;
 
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> arr = new ArrayList<>();
-        Arrays.sort(nums); // Sort the array first
-
-        int n = nums.length;
-
-        for (int i = 0; i < n - 2; i++) {
+        int j,k;
+        List<List<Integer>> arr=new ArrayList<>();
+        
+        Arrays.sort(nums);
+        System.out.println(Arrays.toString(nums));
+        
+        int n=nums.length;
+        
+        
+        for(int i=0;i<n-2;i++){
+            
             if (i > 0 && nums[i] == nums[i - 1]) {
-                // Skip duplicates for the first number
                 continue;
             }
-
-            int j = i + 1, k = n - 1;
-
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
+            
+            
+            j=i+1;
+            k=n-1;
+            while(j<k){
+            int sum=nums[i]+nums[j]+nums[k];
                 
-                if (sum == 0) {
-                    arr.add(Arrays.asList(nums[i], nums[j], nums[k]));
-
-                    // Skip duplicates for the second number
-                    while (j < k && nums[j] == nums[j + 1]) j++;
-                    // Skip duplicates for the third number
-                    while (j < k && nums[k] == nums[k - 1]) k--;
-
+            if(sum==0){
+                arr.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                while(j<k && nums[j]==nums[j+1]){
                     j++;
-                    k--;
-                } else if (sum < 0) {
-                    j++; // Move the left pointer right to increase the sum
-                } else {
-                    k--; // Move the right pointer left to decrease the sum
                 }
+                while(k>j && nums[k]==nums[k-1]){
+                    k--;
+                }
+                j++;
+                k--;
             }
+                else if (sum<0)
+                    j++;
+                else
+                    k--;
+               
+                
+            }
+            
+            
         }
-
+        
         return arr;
+            
     }
 }
+
+    
